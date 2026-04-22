9bcfd1d637237aead5047facf659a730727e3586
Ken Youens-Clark <kyclark@gmail.com>
2026-04-22 19:04:18 +0000

    added pyright

--- nvim/lua/plugins/python.lua
+++ nvim/lua/plugins/python.lua
@@ -1,5 +1,13 @@
 return {
   {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pyright = {},
+      },
+    },
+  },
+  {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
